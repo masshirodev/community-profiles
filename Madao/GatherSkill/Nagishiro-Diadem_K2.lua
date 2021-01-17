114b80b78321a0b53d3dd35a601ad92a75bee55e
masshiro-iro <masshiro.iro.kk@gmail.com>
2021-01-16 23:13:15 -0400

    Adding Nagi and Cypher's skill profiles

---
+++ Madao/GatherSkill/Nagishiro-Diadem_K2.lua
@@ -0,0 +1,33 @@
+local tbl = 
+{
+	
+	{
+		addchain = "1",
+		attemptgt = 6,
+		ephemeral = true,
+		gpgt = 499,
+		id = 3,
+		legend = true,
+		name = "King's Yield II - Blessed Harvest II",
+		normal = true,
+		singlecheck = true,
+		singleuse = true,
+		unspoil = true,
+	},
+	
+	{
+		buffsNot = "1286",
+		chainNot = "1",
+		ephemeral = true,
+		gpgt = 500,
+		id = 10,
+		legend = true,
+		name = "Bountiful Yield II - Bountiful Harvest II",
+		normal = true,
+		unspoil = true,
+	},
+}
+
+
+
+return tbl
